e86a11800c109bc4e5906f33e03da8f76ed175dc
rhannink <1661824+rhannink@users.noreply.github.com>
2026-09-15 17:09:10 +0200

    Add Zigbee effect and speed attributes

--- ZigbeeXYLight.h
+++ ZigbeeXYLight.h
@@ -5,6 +5,19 @@
 
 #if CONFIG_ZB_ENABLED
 
+#define ZIGBEE_EFFECT_CLUSTER_ID 0xFC00
+#define ZIGBEE_EFFECT_ATTR_ID    0x0000
+#define ZIGBEE_SPEED_ATTR_ID     0x0001
+
+enum ZigbeeLedEffect : uint8_t {
+  EFFECT_SOLID = 0,
+  EFFECT_RAINBOW = 1,
+  EFFECT_RAINBOW_CYCLE = 2,
+  EFFECT_COLOR_WIPE = 3,
+  EFFECT_THEATER_CHASE = 4,
+  EFFECT_PULSE = 5
+};
+
 typedef void (*xyLightCallback)(bool state, uint8_t level, uint16_t x, uint16_t y);
 
 class ZigbeeXYLight : public ZigbeeEP {
@@ -17,6 +30,8 @@ public:
   uint8_t getLightLevel() const;
   uint16_t getLightX() const;
   uint16_t getLightY() const;
+  uint8_t getEffect() const;
+  uint8_t getEffectSpeed() const;
 
   void zbAttributeSet(const esp_zb_zcl_set_attr_value_message_t *message) override;
 
@@ -25,6 +40,8 @@ private:
   uint8_t _level;
   uint16_t _x;
   uint16_t _y;
+  uint8_t _effect;
+  uint8_t _effectSpeed;
   xyLightCallback _callback;
 
   void lightChanged();
